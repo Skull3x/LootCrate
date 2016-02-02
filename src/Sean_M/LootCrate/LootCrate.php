213334139b681c11b2778c845bf8c24b6a7335c6
SavionLegendZzz <saviondeavault@gmail.com>
2016-02-02 16:32:36 -0500

    Update LootCrate.php

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -8,24 +8,23 @@ use pocketmine\inventory\Inventory;
 use pocketmine\block\Block;
 use pocketmine\level\Level;
 use pocketmine\math\Vector3;
+use pocketmine\block\Chest;
 
     public function __construct(Main $plugin) {
-
+        $this->config = $plugin->config;
     }
 
     public function onRun($currentTick) {
-       $level = $this->getLevel();
-       $player = $this->player;
-       $x = $this->config["x"];
-       $y = $this->config["y"];
-       $z = $this->config["z"];
+       $level = $this->config->get("world-name");
+       $x = mt_rand(0,255);
+       $y = mt_rand(0,255);
+       $z = mt_rand(0,255);
        $pos = new Vector3($x, $y, $z);
-       $chest = Block::fromString("Chest");
-//     $items = array($this->config["contents"]);         * add fromString
-       $contents = array_rand($items);
-          $level->setBlock($pos, $chest);
-          $chest->setContents($items[$contents]);
-             if($chest->firstEmpty() == -1) {
-                $air = Block::fromString("Air");
-                $level->setBlock($pos, $air);
-    }
+       $level->setBlock($pos, Block::get(54,0));
+       $chest=$level->getTile($pos);
+       $slot=mt_rand(0,225);
+       foreach($this->config->get("items") as $item){
+       $chest->getInventory()->setItem($slot, $item);
+       }
+   /*todo $plugin->getServer()->broadcastMessage("Chest spawned at: ") */
+}
