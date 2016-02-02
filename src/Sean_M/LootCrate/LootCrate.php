b3ea06fa236d4a5b331ab46aacf752b97384c9fd
SavionLegendZzz <saviondeavault@gmail.com>
2016-02-02 16:48:23 -0500

    Update LootCrate.php

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -24,7 +24,7 @@ use pocketmine\block\Chest;
        $chest = $level->getTile($pos);
        $slot = mt_rand(0,27);
        foreach($this->config->get("items") as $item){
-       $chest->getInventory()->setItem($slot, $item->getId());
+       $chest->getInventory()->setItem($slot, $item);
        }
    /*todo $plugin->getServer()->broadcastMessage("Chest spawned at: ") */
 }
