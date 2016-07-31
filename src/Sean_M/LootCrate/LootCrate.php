b7b19391e1e534b267ca71af4cb5e8ef68b57543
CøsʍicSαѵαɢe <jross3740@gmail.com>
2016-07-31 16:53:24 -0400

    Update LootCrate.php

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sean_M\LootCrate;
+namespace Sean_M\LootCrate; //v2
 
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
@@ -13,26 +13,32 @@ use pocketmine\scheduler\PluginTask;
 use pocketmine\plugin\Plugin;
 use Sean_M\LootCrate\Main;
 
-
 class LootCrate extends PluginTask{
 	public $player;
 	public function __construct(Main $owner){
 		parent::__construct($owner);
 		$this->config = $owner->config;
+		
 	}
-
-    public function onRun($currentTick) {
-       $level = $this->config->get("world-name");
-       $x = mt_rand(0,255);
-       $y = mt_rand(0,255);
-       $z = mt_rand(0,255);
-       $pos = new Vector3($x, $y, $z);
-       $level->setBlock($pos, Block::get(54,0));
-       $chest = $level->getTile($pos);
-       $slot = mt_rand(0,27);
-       foreach($this->config->get("items") as $item){
-       $chest->getInventory()->setItem($slot, $item);
-       }
-   /*todo $plugin->getServer()->broadcastMessage("Chest spawned at: ") */
-}
+	
+	public function onRun($currentTick) {
+		$level = $this->config->get("world-name");
+       		$x = mt_rand(0,255);
+       		$y = mt_rand(0,255);
+       		$z = mt_rand(0,255);
+       		$pos = new Vector3($x, $y, $z);
+       		$level->setBlock($pos, Block::get(54,0));
+       		$chest = $level->getTile($pos);
+       		$slot = mt_rand(0,27);
+       		foreach($this->config->get("items") as $item){
+       		$chest->getInventory()->setItem($slot, $item);
+       		$cx = $chest->getX();
+       		$cy = $chest->getY();
+       		$cz = $chest->getZ();
+       		$cpos = new Vector3($cx, $cy, $cz);
+       		if($this->config->get("broadcast.message") == "true"){
+       			$this->getServer()->broadcastMessage($this->config->get("message", str_replace("{CHEST}", $cpos)));
+       		}
+       		}
+	}
 }
